6cfc6b9dc532b8eca607904e1e66f54d45f0dcab
Diana Nguyen <diana.nguyen@epita.fr>
2026-05-19 23:53:42 +0200

    update and save :,3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Container, ThemeProvider, } from '@mui/material';
 import { useEffect } from 'react';
 import theme from './styles/theme';
 import Appbar from './components/appbar';
 import Banner from './components/Banner';
-import Content from './components/Content';
+
+/* Pages */
+import Accueil from './components/Content';
+import Download from "./components/Content/download"
+import Tutos from "./components/Content/tutorials"
+import Wiki from "./components/Content/wiki"
+import Credits from "./components/Content/chromo_team"
 
 
 function App() {
 
   useEffect(()=>{
-    document.title = "React Material UI - Home"
+    document.title = "Nano Project"
   },[]);
 
 
@@ -17,15 +24,21 @@ function App() {
     <ThemeProvider theme={theme}>
       <Container maxWidth="false" disableGutters
       sx={{background: "#fff"}}>
+        <BrowserRouter>
 
-        {/*AppBar*/}
-        <Appbar />
+          {/*AppBar*/}
+          <Appbar />
+          {/*Banner*/}
+          <Banner />
 
-        {/*Banner*/}
-        <Banner />
-
-        {/*Content*/}
-        <Content />
+          <Routes>
+            <Route path='/' element={<Accueil />} />
+            <Route path='/download' element={<Download />} />
+            <Route path='/tutorials' element={<Tutos />} />
+            <Route path='/wiki' element={<Wiki />} />
+            <Route path='/chromo_team' element={<Credits />} />
+          </Routes>
+        </BrowserRouter>
           
       </Container>
     </ThemeProvider>
---
+++ src/components/Content/download.js
@@ -0,0 +1,73 @@
+import {  Box, Container,  Typography, useTheme } from '@mui/material';
+import { Colors } from "../../styles/theme";
+import { BannerContent, BannerText,  } from '../../styles/banner';
+import img2 from "../../assets/Nano.png"
+import Button from '@mui/material/Button';
+
+
+export default function Content(){
+
+    return(
+
+        <Container maxWidth="false" disableGutters>
+
+          <Box display='flex' justifyContent= 'center' width= '100%' height= '40vh' bgcolor={Colors.primary}>
+            <BannerContent>
+              <BannerText>Download</BannerText>
+            </BannerContent>
+          </Box>
+          <Box sx={{ py: 4, px: 20,display: "flex", justifyContent: "flex-end", }}>
+            <Box sx={{flex: 1}}>
+            </Box>
+            
+            <Box sx={{
+              flex: 1,
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "left",
+              px: 6 }}>
+              <Typography variant="h4" fontWeight="bold">
+                A 2D ROGUE LIKE SET IN THE HUMAN BODY
+              </Typography>
+
+              <Typography sx={{mt: 2, pl:4 }}>
+                Pharmacology is history. To save the patient, you must shrink down and strike the evil at its core. The body is your battlefield.
+              </Typography>
+
+              <Typography sx={{ mt: 2 }}>
+                Encore du contenu...<br/>ecd
+              </Typography>
+              <Button
+                component="a"
+                href={img2}
+                download="Nano.png"
+                sx={{ my: 3, color: "black", display: "block" }}
+              >
+                Download
+              </Button>
+            </Box>
+
+            <Box sx={{flex: 2}}>
+              <img src = {img2} style={{ 
+                height: '80%', // L'image fait exactement 30px de haut
+                width: 'auto'   // La largeur s'ajuste pour ne pas écraser l'image
+              }}/>
+            </Box>
+            
+          </Box>
+          <Box sx={{maxwidth: 'sm', height: '30px', bgcolor: Colors.dark}}>
+            <Typography sx={{color: Colors.secondary, textAlign: 'center', fontSize: '1rem'}}>
+
+
+                © 2025 Chromo Team. All right reserved.
+              </Typography>
+          </Box>
+
+
+
+        </Container>
+
+
+        
+    );
+};
